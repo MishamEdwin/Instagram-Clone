870cdcbfae52bd56639f4c8da4baf857599370e3
MishamEdwin <mishamedwin@gmail.com>
2025-05-01 18:02:59 +0530

    Describe what changes you made

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -5,7 +5,7 @@ function Sidebar() {
   return (
     <div className='m-3'>
     <div className='d-flex flex-column gap-3 position-fixed'>
-        <img className="logo-text" src="src\assets\insta_text.png" alt="instagram" />
+        <img className="logo-text" src="assets\insta_text.png" alt="instagram" />
         <div><i className="bi bi-house-door"></i>Home</div>
         <div><i className="bi bi-search"></i>Search</div>
         <div><i className="bi bi-compass"></i>Explore</div>
